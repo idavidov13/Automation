7cc2dcb67e5f5198e4a51945b079716485d59370
idavidov13 <118520300+idavidov13@users.noreply.github.com>
2023-06-28 21:18:06 +0300

    Add files via upload

---
+++ cypress/integration/pageObjects/HomePage.js
@@ -0,0 +1,23 @@
+class HomePage {
+  getEditBox() {
+    return cy.get(":nth-child(1) > .form-control");
+  }
+
+  getTwoWayDataBinding() {
+    return cy.get(":nth-child(4) > .ng-untouched");
+  }
+
+  getGender() {
+    return cy.get("#exampleFormControlSelect1");
+  }
+
+  getEnterpreneurRadioButton() {
+    return cy.get("#inlineRadio3");
+  }
+
+  getShopTab() {
+    return cy.get(":nth-child(2) > .nav-link");
+  }
+}
+
+export default HomePage;
