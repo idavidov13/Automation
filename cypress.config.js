de465939b555a6420e316acbc5dea683f6d74c24
idavidov13 <118520300+idavidov13@users.noreply.github.com>
2023-06-28 21:14:57 +0300

    Add files via upload

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  //declaring global variables
+  env: {
+    url: "https://rahulshettyacademy.com/angularpractice/",
+  },
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    specPattern: "cypress/integration/examples/*.js",
+    specPattern: "cypress/integration/framework/*.js",
+    viewportHeight: 1080,
+    viewportWidth: 1920,
   },
 });
